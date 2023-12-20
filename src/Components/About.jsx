5d02811fc5545b5b6b50e9e91d2ba005d5c80441
mkjilani <mjilani@njit.edu>
2023-12-20 11:08:38 -0500

    Image updated. Closes #20

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,11 +1,11 @@
 
-import img from '../assets/profile.jpg';
+import img from '../assets/PMLogo2.jpg';
 
 const About = () => {
     return (
 
         <div id='About' className="lg:px-56 px-10 lg:py-0 py-20 text-center gap-5 lg:text-start flex lg:flex-row flex-col justify-between lg:gap-28 items-center">
-            <img src={img} width={290} height={290} className='rounded border-2 p-1 ' alt="" />
+            <img src={img} width={290} height={290} className='rounded border-2 p-1 ' alt="Image for Project Management" />
             <div className=" h-full lg:py-40 flex flex-col justify-center lg:items-start items-center text-white">
                 <h1  className="text-[52px] font-semibold mb-8 leading-normal text-blue-500 uppercase">About me</h1>
                 <p className="text-black">20+ year financial industry IT Project management specialist for largescale Operating Systems and Database upgrade projects.</p>
