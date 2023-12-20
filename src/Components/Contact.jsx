e3191c91d714600c6de5e57e72f05dc078357e44
mkjilani <mjilani@njit.edu>
2023-12-20 11:42:09 -0500

    Updated to lighter theme. Closes #28

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -8,11 +8,11 @@ const Contact = () => {
         <h1 className="text-[52px] font-semibold mb-20 leading-normal uppercase text-blue-500">Contact Me</h1>
         <form action="" className="flex flex-col gap-2 lg:w-1/2">
             <div className="lg:flex gap-9">
-                <input className="w-full lg:my-3 my-4 rounded-lg bg-slate-800 p-4 border-2 text-blue-500 text-xl text-slate-500" placeholder="Enter Your Name" type="text" />
-                <input className="w-full lg:my-3 my-4 rounded-lg bg-slate-800 p-4 border-2 text-blue-500  text-xl text-slate-500" placeholder="Enter Your Email" type="text" />
+                <input className="w-full lg:my-3 my-4 rounded-lg bg-white p-4 border-2 text-blue-500 text-xl " placeholder="Enter Your Name" type="text" />
+                <input className="w-full lg:my-3 my-4 rounded-lg bg-white p-4 border-2 text-blue-500  text-xl " placeholder="Enter Your Email" type="text" />
             </div>
-            <textarea className="w-full my-3 rounded-lg bg-slate-800 p-4 border-2 text-blue-500 text-xl text-slate-500" placeholder="Write Your Massage..." name="" id="" cols="20" rows="10"></textarea>
-            <button className="border-2 border-blue-800 text-blue-500 hover:text-blue-900 hover:bg-blue-100 rounded-lg py-4 px-8 my-6 uppercase relative overflow-hidden text-xl text-bold" type="submit">Submit</button>
+            <textarea className="w-full my-3 rounded-lg bg-white p-4 border-2 text-blue-500 text-xl " placeholder="Write Your Massage..." name="" id="" cols="20" rows="10"></textarea>
+            <button className="border-2 bg-slate-500 border-blue-800 text-white hover:text-blue-900  rounded-lg py-4 px-8 my-6 uppercase relative overflow-hidden text-xl text-bold" type="submit">Submit</button>
         </form>
         
        </div>
